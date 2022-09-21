1dee063852895df30d49fd9fc6f361ea9683bdfd
sidharth1805 <sidharth.ramalingam@gmail.com>
2022-09-21 13:16:30 +0530

    modularize write_df program

--- us-venv/write_df.py
+++ us-venv/write_df.py
@@ -1,22 +1,20 @@
-import pandas as pd
-import os
-
-BASE_DIR = 'C:/work_dir/data_copier_data/retail_db_json'
-table_name = 'orders'
-
-file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
-fp = f'{BASE_DIR}/{table_name}/{file_name}'
-
-def main():
-    json_reader = pd.read_json(fp, lines=True, chunksize=1000)
-    conn = 'postgresql://retail_user:itversity@localhost:5452/retail_db'
-    for df in json_reader:
-        min_key = df['order_id'].min()
-        max_key = df['order_id'].max()
-        df.to_sql(table_name, conn, if_exists='append', index=False)
-        print(f'Processed {table_name} with in the range of {min_key} and {max_key}')
+def load_db_table(df, conn, table_name, key):
+    min_key = df[key].min()
+    max_key = df[key].max()
+    df.to_sql(table_name, conn, if_exists='append', index=False)
+    print(f'Loaded data for {table_name} with in the range of {min_key} and {max_key}')
 
 
+if __name__ == '__main__':
+    import pandas as pd
+    import os
 
-if __name__ == "__main__":
-    main()
+    data = [
+        {'user_id': 1, 'user_first_name': 'Scott', 'user_last_name': 'Tiger'},
+        {'user_id': 2, 'user_first_name': 'Donald', 'user_last_name': 'Duck'}
+    ]
+    df = pd.DataFrame(data)
+    configs = dict(os.environ.items())
+    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+
+    load_db_table(df, conn, 'users', 'user_id')
